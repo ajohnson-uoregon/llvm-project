203b582d1a7ff478cab5f5a0816e6aba7d507b6e
Alister Johnson <ajohnson@cs.uoregon.edu>
2022-03-15 21:16:52 -0400

    [clang][AST matchers] new hasExpectedReturnType submatcher for ReturnStmts that looks at the enclosing function's stated return type and matches against that

--- clang/lib/ASTMatchers/Dynamic/Registry.cpp
+++ clang/lib/ASTMatchers/Dynamic/Registry.cpp
@@ -310,6 +310,7 @@ RegistryMaps::RegistryMaps() {
   REGISTER_MATCHER(hasEitherOperand);
   REGISTER_MATCHER(hasElementType);
   REGISTER_MATCHER(hasElse);
+  REGISTER_MATCHER(hasExpectedReturnType);
   REGISTER_MATCHER(hasExplicitSpecifier);
   REGISTER_MATCHER(hasExternalFormalLinkage);
   REGISTER_MATCHER(hasFalseExpression);
