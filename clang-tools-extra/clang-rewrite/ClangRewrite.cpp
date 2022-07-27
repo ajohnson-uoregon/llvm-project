ca5fed1db5c23c031fcbb6c8d8d26ab7c2da8253
Alister Johnson <ajohnson@cs.uoregon.edu>
2022-07-26 23:17:22 -0400

    changing to NCSA license

--- clang-tools-extra/clang-rewrite/ClangRewrite.cpp
+++ clang-tools-extra/clang-rewrite/ClangRewrite.cpp
@@ -1,11 +1,3 @@
-//===--- ClangRewrite.cpp - Command-line tool to rewrite code -------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 // Declares clang::SyntaxOnlyAction.
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
